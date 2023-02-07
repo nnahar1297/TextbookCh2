9a506c3ed58c948fec0f8646f3322a5042cd6ce5
Nazmun Nahar <nnahar@ggc.edu>
2023-02-08 02:39:38 +0600

    Adding code to the Seconds

--- src/Seconds.java
+++ src/Seconds.java
@@ -1,5 +1,37 @@
+import java.util.Scanner;
+
+/**
+ * @class: Seconds
+ * @author: Nazmun Nahar
+ * @course: ITEC2140 - 05, Spring 2023
+ * @version: 1.0
+ * @date: February 7, 2023
+ * description: this program will convert seconds into hours and minutes
+ */
 public class Seconds {
     public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int prompt, second, minute, hour;
 
+        System.out.print("Enter seconds: ");
+        prompt = input.nextInt();
+
+        if(prompt >= 3600){
+            hour = prompt/3600;
+            prompt = prompt - (hour * 3600);
+        }
+        else {
+            hour = 0;
+        }
+        if(prompt >= 60){
+            minute = prompt/60;
+            prompt = prompt - (minute * 60);
+        }
+        else {
+            minute = 0;
+        }
+        second = prompt;
+
+        System.out.println(hour + " hour/s " + minute + " minute/s " + second + " second/s ");
     }
 }
